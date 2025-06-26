e8943fe6c6066160a9c21d2142bad4fcba6e108c
Nafis-Foysal-Ashik <nafisfoisal25@gmail.com>
2025-06-26 11:29:00 +0600

    Variables.js

---
+++ Variables.js
@@ -0,0 +1,29 @@
+//x, y, and z are undeclared variables.
+x=5;
+y=6;
+z=x+y;
+console.log(z);
+console.log("The value of x is "+x);
+console.log("The value of y is "+y);
+console.log("The value of z is "+z);
+
+//The let and const keywords were added to JavaScript in 2015.
+
+let a=10;
+let b=20;
+let c=a+b;
+console.log("The result is : "+c)
+
+//These are constant values and cannot be changed.
+const p = 5;
+const q = 6;
+const r = p + q;
+console.log("The sum is : "+r)
+
+/*
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ 
+Names are case sensitive (y and Y are different variables).
+Reserved words (like JavaScript keywords) cannot be used as names.
+*/
