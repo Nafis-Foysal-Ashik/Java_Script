f9cd61e1a82c425a83c7865e943bfd72544cee89
Nafis-Foysal-Ashik <nafisfoisal25@gmail.com>
2025-06-27 10:36:34 +0600

    Fucntion added

---
+++ Function.js
@@ -0,0 +1,22 @@
+/*
+structure
+function name(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+
+*/
+
+function toCelsius(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+}
+
+let value = toCelsius
+
+// code here can NOT use carName
+
+function myFunction() {
+  let carName = "Volvo";
+  // code here CAN use carName
+}
+
+// code here can NOT use carName
